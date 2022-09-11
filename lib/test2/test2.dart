11caeda8b8b467b71935cfe4721f72a032a03994
Checkerface230 <109644227+Checkerface230@users.noreply.github.com>
2022-09-12 00:43:14 +0800

    NILAGYAN KO NG STATEFUL

--- lib/test2/test2.dart
+++ lib/test2/test2.dart
@@ -1,1 +1,11 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
 
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}//save muna control + saved StatefulWidget
