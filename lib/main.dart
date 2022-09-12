c6d13cfb02d93b252d76587ec2c80e3054174628
Checkerface230 <109644227+Checkerface230@users.noreply.github.com>
2022-09-12 23:17:15 +0800

    debugShowCheckedModeBanner: false

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Picleaf', home: mainPage());
+    return MaterialApp(
+        debugShowCheckedModeBanner: false, title: 'Picleaf', home: mainPage());
   }
 }
