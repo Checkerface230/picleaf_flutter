8b3efa9ff086678f2c996286ff7f553a54aecb66
rogerrabbit20 <75512439+rogerrabbit20@users.noreply.github.com>
2022-09-18 19:30:54 +0800

    small corrections

--- lib/nav_pages/aboutUs.dart
+++ lib/nav_pages/aboutUs.dart
@@ -6,137 +6,138 @@ class AboutUsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: const Text(
-            "Picleaf",
-            style: TextStyle(
-                color: Color.fromRGBO(102, 204, 102, 100),
-                fontWeight: FontWeight.bold),
-          ),
-          backgroundColor: Colors.white,
-          shadowColor: const Color.fromARGB(255, 95, 94, 94),
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          "Picleaf",
+          style: TextStyle(
+              color: Color.fromRGBO(102, 204, 102, 100),
+              fontWeight: FontWeight.bold),
         ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-              const SizedBox(
-                height: 20,
+        backgroundColor: Colors.white,
+        shadowColor: const Color.fromARGB(255, 95, 94, 94),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              padding: const EdgeInsets.fromLTRB(20, 20, 20, 10),
+              child: const Text(
+                'About PicLeaf',
+                style: TextStyle(
+                    fontSize: 30,
+                    fontFamily: 'RobotoBold',
+                    color: Color.fromRGBO(102, 204, 102, 100)),
+                textAlign: TextAlign.center,
               ),
-              Container(
-                padding: const EdgeInsets.fromLTRB(20, 20, 20, 10),
-                child: const Text(
-                  'About PicLeaf',
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Container(
+              padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+              child: const Text(
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eu lacus vitae ex congue euismod sed venenatis ex. Curabitur ornare quis tortor eu volutpat. Integer euismod ex et massa pretium eleifend. Quisque mollis aliquet metus, id hendrerit orci ultricies in. Duis enim purus, sagittis eget turpis quis, egestas iaculis ex.',
+                style: TextStyle(
+                    fontSize: 18,
+                    fontFamily: 'RobotoBold',
+                    color: Colors.black),
+                textAlign: TextAlign.justify,
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Container(
+              padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+              child: const Text('App Creators',
                   style: TextStyle(
                       fontSize: 30,
                       fontFamily: 'RobotoBold',
                       color: Color.fromRGBO(102, 204, 102, 100)),
-                  textAlign: TextAlign.center,
-                ),
+                  textAlign: TextAlign.center),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            const CircleAvatar(
+              backgroundColor: Color.fromRGBO(102, 204, 102, 100),
+              radius: 60,
+              child: CircleAvatar(
+                radius: 55,
+                backgroundImage: AssetImage('assets/images/Fritz_pic.png'),
               ),
-              const SizedBox(
-                height: 10,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Container(
+              padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+              child: const Text(
+                'Fritz D. Lamparas',
+                style: TextStyle(
+                    fontSize: 18,
+                    fontFamily: 'RobotoBold',
+                    color: Colors.black),
+                textAlign: TextAlign.justify,
               ),
-              Container(
-                padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
-                child: const Text(
-                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eu lacus vitae ex congue euismod sed venenatis ex. Curabitur ornare quis tortor eu volutpat. Integer euismod ex et massa pretium eleifend. Quisque mollis aliquet metus, id hendrerit orci ultricies in. Duis enim purus, sagittis eget turpis quis, egestas iaculis ex.',
-                  style: TextStyle(
-                      fontSize: 18,
-                      fontFamily: 'RobotoBold',
-                      color: Colors.black),
-                  textAlign: TextAlign.justify,
-                ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            const CircleAvatar(
+              backgroundColor: Color.fromRGBO(102, 204, 102, 100),
+              radius: 60,
+              child: CircleAvatar(
+                radius: 55,
+                backgroundImage: AssetImage('assets/images/JC_pic.png'),
               ),
-              const SizedBox(
-                height: 10,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Container(
+              padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+              child: const Text(
+                'John Carlo P. Hermosa',
+                style: TextStyle(
+                    fontSize: 18,
+                    fontFamily: 'RobotoBold',
+                    color: Colors.black),
+                textAlign: TextAlign.justify,
               ),
-              Container(
-                padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
-                child: const Text('App Creators',
-                    style: TextStyle(
-                        fontSize: 30,
-                        fontFamily: 'RobotoBold',
-                        color: Color.fromRGBO(102, 204, 102, 100)),
-                    textAlign: TextAlign.center),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            const CircleAvatar(
+              backgroundColor: Color.fromRGBO(102, 204, 102, 100),
+              radius: 60,
+              child: CircleAvatar(
+                radius: 55,
+                backgroundImage: AssetImage('assets/images/Russel_pic.png'),
               ),
-              const SizedBox(
-                height: 25,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Container(
+              padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+              child: const Text(
+                'Russel Ivan B. Bataller',
+                style: TextStyle(
+                    fontSize: 18,
+                    fontFamily: 'RobotoBold',
+                    color: Colors.black),
+                textAlign: TextAlign.justify,
               ),
-              const CircleAvatar(
-                backgroundColor: Color.fromRGBO(102, 204, 102, 100),
-                radius: 120,
-                child: CircleAvatar(
-                  radius: 110,
-                  backgroundImage: AssetImage('assets/images/Fritz_pic.png'),
-                ),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Container(
-                padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
-                child: const Text(
-                  'Fritz D. Lamparas',
-                  style: TextStyle(
-                      fontSize: 18,
-                      fontFamily: 'RobotoBold',
-                      color: Colors.black),
-                  textAlign: TextAlign.justify,
-                ),
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              const CircleAvatar(
-                backgroundColor: Color.fromRGBO(102, 204, 102, 100),
-                radius: 120,
-                child: CircleAvatar(
-                  radius: 110,
-                  backgroundImage: AssetImage('assets/images/JC_pic.png'),
-                ),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Container(
-                padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
-                child: const Text(
-                  'John Carlo P. Hermosa',
-                  style: TextStyle(
-                      fontSize: 18,
-                      fontFamily: 'RobotoBold',
-                      color: Colors.black),
-                  textAlign: TextAlign.justify,
-                ),
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              const CircleAvatar(
-                backgroundColor: Color.fromRGBO(102, 204, 102, 100),
-                radius: 120,
-                child: CircleAvatar(
-                  radius: 110,
-                  backgroundImage: AssetImage('assets/images/Russel_pic.png'),
-                ),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Container(
-                padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
-                child: const Text(
-                  'Russel Ivan B. Bataller',
-                  style: TextStyle(
-                      fontSize: 18,
-                      fontFamily: 'RobotoBold',
-                      color: Colors.black),
-                  textAlign: TextAlign.justify,
-                ),
-              ),
-            ],
-          ),
-        ));
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/nav_pages/home.dart
+++ lib/nav_pages/home.dart
@@ -97,31 +97,31 @@ class _HomePageState extends State<homePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: const Text(
-            "Picleaf",
-            style: TextStyle(
-                color: Color.fromRGBO(102, 204, 102, 100),
-                fontWeight: FontWeight.bold),
-          ),
-          backgroundColor: Colors.white,
-          shadowColor: const Color.fromARGB(255, 95, 94, 94),
-          actions: [
-            IconButton(
-              onPressed: () {
-                // method to show the search bar
-                showSearch(
-                    context: context,
-                    // delegate to customize the search bar
-                    delegate: CustomSearchDelegate());
-              },
-              icon: const Icon(Icons.search, color: Colors.black),
-            )
-          ],
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          "Picleaf",
+          style: TextStyle(
+              color: Color.fromRGBO(102, 204, 102, 100),
+              fontWeight: FontWeight.bold),
         ),
-        body: SingleChildScrollView(
-            child: Column(
+        backgroundColor: Colors.white,
+        shadowColor: const Color.fromARGB(255, 95, 94, 94),
+        actions: [
+          IconButton(
+            onPressed: () {
+              // method to show the search bar
+              showSearch(
+                  context: context,
+                  // delegate to customize the search bar
+                  delegate: CustomSearchDelegate());
+            },
+            icon: const Icon(Icons.search, color: Colors.black),
+          )
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             const SizedBox(
@@ -142,7 +142,7 @@ class _HomePageState extends State<homePage> {
               padding: const EdgeInsets.fromLTRB(20, 0, 20, 10),
               child: const Text('Find out what is wrong with your plant!',
                   style: TextStyle(
-                      fontSize: 20,
+                      fontSize: 18,
                       fontFamily: 'RobotoBold',
                       color: Colors.black),
                   textAlign: TextAlign.left),
@@ -151,7 +151,7 @@ class _HomePageState extends State<homePage> {
               height: 30,
             ),
             Container(
-              padding: const EdgeInsets.fromLTRB(20, 0, 20, 10),
+              padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
               margin: const EdgeInsets.symmetric(horizontal: 20.0),
               color: const Color.fromRGBO(102, 204, 102, 100),
               child: const Text('List of Plants',
@@ -202,6 +202,8 @@ class _HomePageState extends State<homePage> {
               ],
             ),
           ],
-        )));
+        ),
+      ),
+    );
   }
 }
