7e4a4fa0d51d345fbcf290ce9b276c73645ae88d
Checkerface230 <109644227+Checkerface230@users.noreply.github.com>
2022-10-21 22:22:51 +0800

    card updates all

--- lib/widgets/card.dart
+++ lib/widgets/card.dart
@@ -2,21 +2,63 @@ import 'package:flutter/material.dart';
 
 class ListViewCard extends StatelessWidget {
   final String title;
-  final Color? textColor;
-  final void Function()? onPressed;
-  final Image image;
+  final void Function()? onTap;
+  final String imageOfPlant; //Change to String
 
-  const ListViewCard(
-      {super.key,
-      required this.title,
-      required this.onPressed,
-      required this.image,
-      this.textColor});
+  const ListViewCard({
+    super.key,
+    required this.title,
+    required this.onTap,
+    required this.imageOfPlant,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const Card(
-      color: Color.fromARGB(255, 75, 175, 78),
-    );
+    return Card(
+      color: const Color.fromARGB(255, 75, 175, 78),
+      elevation: 1,
+      margin: const EdgeInsets.all(8),
+      semanticContainer: true,
+      clipBehavior: Clip.antiAliasWithSaveLayer,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: InkWell(
+        splashColor: Colors.lightGreenAccent.withAlpha(30),
+        onTap: onTap,
+
+        //sizedBox of the card
+        child: Stack(
+          children: <Widget>[
+            //image of the card
+            Image.asset(
+              imageOfPlant,
+              height: 200,
+              width: 200,
+              fit: BoxFit.cover,
+            ),
+            Positioned(
+              bottom: 0,
+              right: 0,
+              left: 0,
+              child: SizedBox(
+                height: 50,
+                width: 150,
+                child: Center(
+                  child: Text(
+                    title,
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                        fontSize: 19,
+                        fontFamily: 'RobotoMedium',
+                        color: Color(0xffeeeeee)), // textstyle
+                  ),
+                ),
+              ),
+            ), //text //SizedBox
+          ], // <widget>[]
+        ), // column
+      ), //inkwell
+    ); // card
   }
 }
